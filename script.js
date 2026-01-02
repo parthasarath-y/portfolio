e930f9ddfb9c18d7b65b19cbed3d89e97f240a51
Parth <166246154+parthasarath-y@users.noreply.github.com>
2026-01-02 05:42:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ function toggleProjects(id) {
+      const grids = document.querySelectorAll('.project-grid');
+      grids.forEach(g => g.style.display = 'none');
+      const target = document.getElementById(id + '-projects');
+      if (target) target.style.display = 'grid';
+    }
+
+
+    const modal = document.getElementById('modal');
+    const openBtn = document.querySelector('.open-modal-btn');
+    const closeBtn = document.querySelector('.close-btn');
+
+    openBtn.addEventListener('click', () => {
+      modal.classList.add('active');
+    });
+
+    
+    closeBtn.addEventListener('click', () => {
+      modal.classList.remove('active');
+    });
+
+    // Close when clicking outside
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.remove('active');
+      }
+    });
